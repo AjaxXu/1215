9a03ae3ed323b8713744ad6dc1c7cf8bb7cbfcf9
AjaxXu <louis@MacBook.local>
2015-04-14 10:41:12 +0800

    lalal

--- blog.py
+++ blog.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask
+from flask import Flask, session, request, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
